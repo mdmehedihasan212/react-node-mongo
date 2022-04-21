1aff380ed3ef4849e3bd294cc2db36848ec6f3ea
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-21 20:07:18 +0600

    display data delete btn click add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Adduser from '../Adduser/Adduser';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
+    const handleUserDelete = id => {
+        const process = window.confirm('Are you sure delete');
+        if (process) {
+            console.log('this is delete user', id);
+        }
+    }
+
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Available now: {users.length}</h1>
+            <Adduser></Adduser>
+            <ul>
+                {
+                    users.map(user => <li key={user._id}>
+                        Name: {user.name}:: Email: {user.email}
+                        <button onClick={() => handleUserDelete(user._id)}>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
